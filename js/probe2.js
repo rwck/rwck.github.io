7960d61cf1369edf596c26a9d85025cbd52a4105
Donald Wade <rwckdw@gmail.com>
2015-10-13 21:01:59 +1100

    got North direction row probing function working; this should be the basis for all other row probing functions.

--- js/probe2.js
+++ js/probe2.js
@@ -21,34 +21,43 @@ function addArrayItems(array1, array2) {
 
 var moveToCoords = addArrayItems([12, 33], north)
 
-var a = [1, 2,3], b = [10, 20, 30];
-a.map(function(item, i){
-  return item + b[i] });
+var a = [1, 2, 3],
+  b = [10, 20, 30];
+a.map(function(item, i) {
+  return item + b[i]
+});
 [11, 22, 33]
 
 
-function probeNorth() {
-
-}
 
 
 
-var rowProbed = [];
 
-var rowLength = Math.sqrt(Object.keys(testObject).length);
 
- {
 
-}
-function testColourOfNorth(x, y) {
-  $.each(rowLength, function(i, val) {
-    console.log(testObject[cellAt(x, y)].colour);
-    var colourToPush = (testObject[cellAt(x, y)].colour);
-    var nextCoords =  addArrayItems([x, y], north);
-    console.log(nextCoords);
-    rowProbed.push(colourToPush);
-    console.log(rowProbed);
+
+
+function repeat(fn, times) {
+  for (var i = 0; i < times; i++) {
+    fn;
   }
 }
 
-testObject
+
+function testColourOfNorth(x, y) {
+  var rowLength = Math.sqrt(Object.keys(testObject).length);
+  var rowProbed = [];
+  for (var i = 0; i < rowLength; i++) {
+    var startingColour = (testObject[cellAt(x, y)].colour);
+    rowProbed.push(startingColour);
+    var nextCoords = (addArrayItems([x, y], north));
+    var x = nextCoords[0];
+    var y = nextCoords[1];
+    var boo = cellAt(x, y);
+    try {
+      var nextCellColour = testObject[boo].colour;
+    } catch (error) {
+    }
+  }
+  return rowProbed;
+}
